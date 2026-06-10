322de62ee50f8814f9f7e57d663385bbda8cdbb4
Mridul  Jain <mridul.jain@cginfinity.com>
2026-06-10 12:58:44 +0530

    changes in update api

--- java-component/src/main/java/com/example/products/ProductService.java
+++ java-component/src/main/java/com/example/products/ProductService.java
@@ -34,7 +34,7 @@ public class ProductService {
 
     public Product update(Long id, ProductRequest request) {
         Product product = findById(id);
-        product.setName(request.getName());
+        product.setName("NewUpdatedProduct" +request.getName());
         product.setPrice(request.getPrice());
         return product;
     }
