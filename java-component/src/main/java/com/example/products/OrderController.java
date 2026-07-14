248670ad19e57bb0737bd5e51bb1c540c544a626
Mridul Jain <itsme.mriduljain@gmail.com>
2026-07-14 22:42:52 +0530

    changes

--- java-component/src/main/java/com/example/products/OrderController.java
+++ java-component/src/main/java/com/example/products/OrderController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/order")
+@RequestMapping("/api/v1/orderss")
 public class OrderController {
 
     private final OrderService orderService;
